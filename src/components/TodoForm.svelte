7159009803bccc50a8b3d57fcc0a5ee57e1c4181
admdl <admdl@play02199.soton.ac.uk>
2022-11-10 00:32:11 +0000

    Completed

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,7 +1,16 @@
-<form class="my-6">
+<script>
+    let todo = '';
+    import {addTodo} from "../stores/todoStore.js"
+    const handleSubmit = () => {
+        addTodo(todo);
+        todo = '';
+        console.log("submitting")
+    }
+</script>
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
     <div class="flex flex-col text-sm mb-2">
          <label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
-            <input type="text" name='todo' placeholder="Var gör du?/Was macht du?" class="apperaance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"/>
+            <input type="text" bind:value={todo} name='todo' placeholder="Var gör du?/Was macht du?" class="apperaance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"/>
     </div>
     <button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Submit</button>
 </form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 import TodoForm from "../components/TodoForm.svelte";
+import Todo from "../components/Todo.svelte";
+import {todos} from "../stores/todoStore.js"
 </script>
 
 <main>
@@ -7,4 +9,7 @@ import TodoForm from "../components/TodoForm.svelte";
     <h1 class="text-2xl font-bold text-center text-gray-800">Att göra</h1>
     <h1 class="text-2xl font-bold text-center text-gray-800">Machen</h1>
     <TodoForm/>
+    {#each $todos as todo}
+        <Todo todo={todo} index={todo.id}/>
+    {/each}
 </main>
